13a444165eb27749cd7e10a3109bf7c1232addcc
Wissal Loutfi <147111691+loutfiwissal@users.noreply.github.com>
2023-11-20 00:29:43 +0100

    Update Player.java

---
+++ Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    String name ;
+
+    short age ;
+
+    double rank ;
+
+    String team;
+
+
+    //method to provide player information:
+    void info(){
+        System.out.println ("The player name is:"+name) ;
+        System.out.println ("His age is:"+age+"year") ;
+        System.out.println ("His rank is:"+rank) ;
+        System.out.println ("And his team is:"+team) ;
+    }
+
+}
+
